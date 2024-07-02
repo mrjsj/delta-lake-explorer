e955accd87dd6823a6db9e20abb7848b569d83ee
jsj <jsj>
2024-07-02 22:54:57 +0200

    monkey brain logic fix

--- modules/sidebar.py
+++ modules/sidebar.py
@@ -10,7 +10,7 @@ def load_sidebar():
             key="enable_query_parsing",
             help="""Enables parsing `"catalog"."schema"."table"` to `delta_scan('abfss://{root_path}/{catalog}/{schema}/{table}')` for all `FROM` and `JOIN` statements."""
         )
-        st.checkbox("Show parsed query", value=False if not st.session_state["enable_query_parsing"] else True, key="show_parsed_query", help="Shows the parsed query when running a query.", disabled=not st.session_state["enable_query_parsing"])
+        st.checkbox("Show parsed query", value=st.session_state["enable_query_parsing"], key="show_parsed_query", help="Shows the parsed query when running a query.", disabled=not st.session_state["enable_query_parsing"])
 
         st.checkbox(
             label="Show query time",
